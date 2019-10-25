2759655bad60e923fa802779b18be912b50a4ac3
RoshanSalian <roshansalian1997@gmail.com>
2019-10-25 08:18:36 +0530

    Added Readme and a few additions

--- Age.py
+++ Age.py
@@ -1,15 +1,15 @@
 import datetime
 import sys
-def calculate_age(age):				#Calculating  Age 
+def calculate_age(age):				#Calculating  Age
 	today = datetime.date.today()
 	try:
-		day = abs(today.day - int(age[0:1]))  
-		month = abs(today.month - int(age[2:3]))  
-		year = abs(today.year - int(age[4:8]))  
+		day = abs(today.day - int(age[0:1]))
+		month = abs(today.month - int(age[2:3]))
+		year = abs(today.year - int(age[4:8]))
 		#print([str(day),str(month),str(year)])
 		return ([str(day),str(month),str(year)])
 	except:
-		help()		
+		help()
 
 def format_date(accepted_age):		#convet date dd/mm/yyyy to ddmmyyyy
 	if (len(accepted_age) == 10 and accepted_age[2] == "/" and accepted_age[5] == "/"):
@@ -19,18 +19,28 @@ def format_date(accepted_age):		#convet date dd/mm/yyyy to ddmmyyyy
 			#print accepted_age
 			return accepted_age
 	help()
-	
+
 def help():
+	print("Print a valid Date of Birth")
 	print """Invalid Date Format\nUse This format : python Age.py dd/mm/yyyy """
-	
+
 def main():
 		if (len(sys.argv) == 2):
 			date = format_date(sys.argv[1])
 			if date is not None:
-				actual_age = calculate_age(date) 
+				actual_age = calculate_age(date)
 				print("{0} Year {1} Month and {2} Day".format(actual_age[2],actual_age[1],actual_age[0]))
+				if actual_age[2]<=9:
+					print("Your a kid")
+				elif 10<=actual_age[2]<=13:
+					print("Early Adolescence")
+				elif 14<=actual_age[2]<=17:
+					print("Middle Adolescence")
+				elif 18<= actual_age[2] <=21:
+					print("Late Adolescents")
+				else:
+					print("Your and adult now!")
 		else:
 			help()
-		
-main()
 
+main()
--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Age Calculator
-A  Basic Guithub repository
-Which calculate age from given Date of Birth 
+Basic python program to calculate age give date of birth in dd/mm/yyyy format.
+
+# Prerequiste
+Python2
+
+# Run the program
+Create a local copy of the project in your machine, navigate to the directory in the terminal and run the program in terminal in the format: python2 Age.py dd/mm/yyyy.
+
+```
+python2 Age.py 05/07/1997
+```
